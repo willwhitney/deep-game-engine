91ad60f93912a2b8397c6e34b77826cf38d2d6b5
Will Whitney <wwhitney@mit.edu>
2015-04-06 16:51:03 -0400

    play with learning rates

--- reconstruction_runner.py
+++ reconstruction_runner.py
@@ -21,44 +21,27 @@ jobs = [
             'datasetdir': 'dataset-trained',
             'num_train_batches': 70000,
             'num_test_batches': 8000,
-            'grayscale': True
+            'learning_rate': -.00005
         },
         {
             'datasetdir': 'dataset-trained',
             'num_train_batches': 70000,
             'num_test_batches': 8000,
-            'dim_hidden': 400,
-            'grayscale': True
+            'learning_rate': -.00001
         },
         {
             'datasetdir': 'dataset-trained',
             'num_train_batches': 70000,
             'num_test_batches': 8000,
-            'dim_hidden': 40,
-            'grayscale': True
+            'learning_rate': -.00005,
+            'momentum_decay': 0.05
         },
         {
             'datasetdir': 'dataset-trained',
             'num_train_batches': 70000,
             'num_test_batches': 8000,
-            'feature_maps': 144,
-            'grayscale': True
-        },
-        {
-            'datasetdir': 'dataset-trained',
-            'num_train_batches': 70000,
-            'num_test_batches': 8000,
-            'dim_hidden': 400,
-            'feature_maps': 128,
-            'grayscale': True
-        },
-        {
-            'datasetdir': 'dataset-trained',
-            'num_train_batches': 70000,
-            'num_test_batches': 8000,
-            'dim_hidden': 40,
-            'feature_maps': 48,
-            'grayscale': True
+            'learning_rate': -.00001,
+            'momentum_decay': 0.05
         }
     ]
 
