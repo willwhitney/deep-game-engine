d554c707b2db4e7b9c1847cbee583830598f8f16
Will Whitney <wwhitney@mit.edu>
2015-04-22 19:14:37 -0400

    built a runner for DQN data generation

---
+++ DQN_runner.py
@@ -0,0 +1,96 @@
+import os
+import sys
+
+dry_run = '--dry-run' in sys.argv
+local   = '--local' in sys.argv
+detach  = '--detach' in sys.argv
+
+if not os.path.exists("slurm_logs"):
+    os.makedirs("slurm_logs")
+
+if not os.path.exists("slurm_scripts"):
+    os.makedirs("slurm_scripts")
+
+networks_prefix = "networks/"
+networks_suffix = ".t7"
+
+jobs = [
+        {
+            'name': 'my_first_DQN',
+            'import': None,
+            'rom': 'breakout',
+            'learn': False,
+            'steps': '10000',
+        },
+        # {
+        #     'name': 'my_first_DQN',
+        #     'import': 'DQN_saving_params',
+        #     'rom': 'breakout',
+        #     'learn': False,
+        #     'steps': '100000',
+        # },
+    ]
+
+if dry_run:
+    print "NOT starting jobs:"
+else:
+    print "Starting jobs:"
+
+for job in jobs:
+    assert 'name' in job
+    assert 'import' in job
+    assert 'rom' in job
+    assert 'learn' in job
+    assert 'steps' in job
+
+    name = job.pop('name', None)
+    import_file = job.pop('import', None)
+    rom = job.pop('rom', None)
+    learn = job.pop('learn', None)
+    steps = job.pop('steps', None)
+
+    assert job == {}
+
+    # import_string = ''
+    if import_file != None:
+        import_string = "-network '" + networks_prefix + import_file + networks_suffix + "'"
+    else:
+        import_string = ""
+
+    jobcommand = "th train_agent.lua $args -name '{name}' {import_string} -steps {steps} {learn}".format(
+        name = name,
+        import_string = import_string,
+        steps = steps,
+        learn = '-learn' if learn else '')
+
+    script_path = 'run_gpu_' + name
+    os.system('cp run_gpu_template ' + script_path)
+
+    with open(script_path, 'a') as script_file:
+        script_file.write('\n')
+        script_file.write(jobcommand)
+        script_file.write('\n')
+
+
+    print(jobcommand + " " + rom)
+    if local:
+        if not dry_run:
+            if detach:
+                os.system('bash ' + script_path + ' ' + rom + ' 2> slurm_logs/' + name + '.err 1> slurm_logs/' + name + '.out &')
+            else:
+                os.system('bash ' + script_path + ' ' + rom)
+
+    else:
+        with open('slurm_scripts/' + name + '.slurm', 'w') as slurmfile:
+            slurmfile.write("#!/bin/bash\n")
+            slurmfile.write("#SBATCH --job-name"+"=" + name + "\n")
+            slurmfile.write("#SBATCH --output=slurm_logs/" + name + ".out\n")
+            slurmfile.write("#SBATCH --error=slurm_logs/" + name + ".err\n")
+            slurmfile.write('./' + script_path + ' ' + rom)
+
+        if not dry_run:
+            os.system("sbatch -N 1 -c 1 --gres=gpu:1 -p gpu --time=6-23:00:00 slurm_scripts/" + name + ".slurm &")
+
+
+
+
--- train_agent.lua
+++ train_agent.lua
@@ -14,6 +14,9 @@ cmd:text('Train Agent in Environment:')
 cmd:text()
 cmd:text('Options:')
 
+cmd:option('-learn', false,
+    'whether the network should learn or just make data')
+
 cmd:option('-framework', '', 'name of training framework')
 cmd:option('-env', '', 'name of environment to use')
 cmd:option('-game_path', '', 'path to environment file (ROM)')
@@ -31,13 +34,13 @@ cmd:option('-agent_params', '', 'string of agent parameters')
 cmd:option('-seed', 1, 'fixed input seed for repeatable experiments')
 cmd:option('-saveNetworkParams', true,
            'saves the agent network in a separate file')
-cmd:option('-prog_freq', 5*10^3, 'frequency of progress output')
+cmd:option('-prog_freq', 10^4, 'frequency of progress output')
 cmd:option('-save_freq', 5*10^4, 'the model is saved every save_freq steps')
-cmd:option('-eval_freq', 10^4, 'frequency of greedy evaluation')
+cmd:option('-eval_freq', 2*10^4, 'frequency of greedy evaluation')
 cmd:option('-save_versions', 0, '')
 
-cmd:option('-steps', 10^5, 'number of training steps to perform')
-cmd:option('-eval_steps', 10^5, 'number of evaluation steps')
+cmd:option('-steps', 50000000, 'number of training steps to perform')
+cmd:option('-eval_steps', 250000, 'number of evaluation steps')
 
 cmd:option('-verbose', 2,
            'the higher the level, the more information is printed to screen')
@@ -45,86 +48,20 @@ cmd:option('-threads', 1, 'number of BLAS threads')
 cmd:option('-gpu', -1, 'gpu flag')
 
 cmd:text()
-
+-- print(arg)
 local opt = cmd:parse(arg)
-
-local network_imported = (opt.network:sub(opt.network:len() - 2) == '.t7')
+-- print(opt)
+-- local network_imported = (opt.network:sub(opt.network:len() - 2) == '.t7')
 
 --- General setup.
 game_env, game_actions, agent, opt = setup(opt)
 
-print("agent: ".. tostring(agent))
-print("agent network: ".. tostring(agent.network))
-print(agent.network.modules[3].weight[1][1])
+-- print("agent: ".. tostring(agent))
+-- print("agent network: ".. tostring(agent.network))
+-- print(agent.network.modules[3].weight[1][1])
 -- print("agent w: ".. tostring(agent.w))
 
 -- for k, v in pairs(agent) do print(k) end
----------------------------------------------
---
-agent.numSteps = 35000000
-
-local step = 0
-local screen, reward, terminal = game_env:getState()
-os.execute('mkdir -p tempscreens')
-
-while step < opt.steps do
-   step = step + 1
-
-
-
-   torch.save('tempscreens/step'..step, screen)
-   
-   if not terminal then
-      local action_index = agent:greedy(agent:preprocess(screen):float())
-      screen, reward, terminal = game_env:step(game_actions[action_index], true)
-   else
-      if opt.random_starts > 0 then
-	 screen, reward, terminal = game_env:nextRandomGame()
-      else
-	 screen, reward, terminal = game_env:newGame()
-      end
-   end
-end
---]]
-----------------------------------------------
-
---[[
-
--- if network_imported then
---     local loaded_w = torch.load(opt.network:sub(1, opt.network:len() - 3) .. '.params.t7', 'ascii').network
---     agent.w = loaded_w:cuda()
--- end
-
--- override print to always flush the output
-local old_print = print
-local print = function(...)
-    old_print(...)
-    io.flush()
-end
-
-local learn_start = agent.learn_start
-local start_time = sys.clock()
-local reward_counts = {}
-local episode_counts = {}
-local time_history = {}
-local v_history = {}
-local qmax_history = {}
-local td_history = {}
-local reward_history = {}
-local step = 0
-time_history[1] = 0
-
-local total_reward
-local nrewards
-local nepisodes
-local episode_reward
-
-local screen, reward, terminal = game_env:getState()
-
-
-local dataset_output_dir = 'dataset-dummy'
-os.execute('mkdir -p ' .. dataset_output_dir .. '/test')
-os.execute('mkdir -p ' .. dataset_output_dir .. '/train')
 
 local batch_size = 30
 local images = torch.Tensor(batch_size, 3, 210, 160)
@@ -135,16 +72,22 @@ local completed_train_batches = 0
 local completed_test_batches = 0
 local test_fraction = 10
 
-print("Iteration ..", step)
-while step < opt.steps do
-    step = step + 1
-    local action_index = agent:perceive(reward, screen, terminal)
+local dataset_output_dir = 'dataset_' .. opt.name
+os.execute('mkdir -p ' .. dataset_output_dir .. '/test')
+os.execute('mkdir -p ' .. dataset_output_dir .. '/train')
 
-    -- only save every now and then,
-    -- but make sure the batches stay whole! (sequential)
-    local save_flag = false
-    if intra_batch_index == 1 then
-        if torch.random(1000) ~= 1 then
+if not opt.learn then ---------------------------------------------
+    local step = 0
+    local screen, reward, terminal = game_env:getState()
+
+    while step < opt.steps do
+     step = step + 1
+
+     local action_index = agent:greedy(agent:preprocess(screen):float())
+
+     local save_flag = true
+     if intra_batch_index == 1 then
+        if torch.random(1) ~= 1 then
             save_flag = false
         end
     end
@@ -166,142 +109,227 @@ while step < opt.steps do
             end
 
             intra_batch_index = 1
-            -- images = torch.Tensor(batch_size, 3, 210, 160)
-            -- actions = torch.Tensor(batch_size)
+                -- images = torch.Tensor(batch_size, 3, 210, 160)
+                -- actions = torch.Tensor(batch_size)
+            end
         end
-    end
 
-
-    -- game over? get next game!
-    if not terminal then
-        screen, reward, terminal = game_env:step(game_actions[action_index], true)
-    else
-        if opt.random_starts > 0 then
-            screen, reward, terminal = game_env:nextRandomGame()
+        if not terminal then
+            screen, reward, terminal = game_env:step(game_actions[action_index], true)
         else
-            screen, reward, terminal = game_env:newGame()
-        end
-    end
-
-    if step % opt.prog_freq == 0 then
-        -- print(screen)
-        assert(step==agent.numSteps, 'trainer step: ' .. step ..
-                ' & agent.numSteps: ' .. agent.numSteps)
-        print("Steps: ", step)
-        agent:report()
-        collectgarbage()
-    end
-
-    if step%1000 == 0 then collectgarbage() end
-
-    if step % opt.eval_freq == 0 and step > learn_start then
-
-        screen, reward, terminal = game_env:newGame()
-
-        total_reward = 0
-        nrewards = 0
-        nepisodes = 0
-        episode_reward = 0
-
-        local eval_time = sys.clock()
-        for estep=1,opt.eval_steps do
-            local action_index = agent:perceive(reward, screen, terminal, true, 0.05)
-
-            -- Play game in test mode (episodes don't end when losing a life)
-            screen, reward, terminal = game_env:step(game_actions[action_index])
-
-            if estep%1000 == 0 then collectgarbage() end
-
-            -- record every reward
-            episode_reward = episode_reward + reward
-            if reward ~= 0 then
-               nrewards = nrewards + 1
-            end
-
-            if terminal then
-                total_reward = total_reward + episode_reward
-                episode_reward = 0
-                nepisodes = nepisodes + 1
+            if opt.random_starts > 0 then
                 screen, reward, terminal = game_env:nextRandomGame()
+            else
+                screen, reward, terminal = game_env:newGame()
+            end
+        end
+    end
+
+
+
+else ----------------------------------------------
+
+    --
+
+    -- if network_imported then
+    --     local loaded_w = torch.load(opt.network:sub(1, opt.network:len() - 3) .. '.params.t7', 'ascii').network
+    --     agent.w = loaded_w:cuda()
+    -- end
+
+    -- override print to always flush the output
+    local old_print = print
+    local print = function(...)
+        old_print(...)
+        io.flush()
+    end
+
+    local learn_start = agent.learn_start
+    local start_time = sys.clock()
+    local reward_counts = {}
+    local episode_counts = {}
+    local time_history = {}
+    local v_history = {}
+    local qmax_history = {}
+    local td_history = {}
+    local reward_history = {}
+    local step = 0
+    time_history[1] = 0
+
+    local total_reward
+    local nrewards
+    local nepisodes
+    local episode_reward
+
+    local screen, reward, terminal = game_env:getState()
+
+    print("Iteration ..", step)
+    while step < opt.steps do
+        step = step + 1
+        local action_index = agent:perceive(reward, screen, terminal)
+
+        -- only save every now and then,
+        -- but make sure the batches stay whole! (sequential)
+        local save_flag = false
+        if intra_batch_index == 1 then
+            if torch.random(1000) ~= 1 then
+                save_flag = false
             end
         end
 
-        eval_time = sys.clock() - eval_time
-        start_time = start_time + eval_time
-        agent:compute_validation_statistics()
-        local ind = #reward_history+1
-        total_reward = total_reward/math.max(1, nepisodes)
+        if save_flag then
+            images[intra_batch_index] = screen:float()
+            actions[intra_batch_index] = action_index
+            intra_batch_index = intra_batch_index + 1
 
-        if #reward_history == 0 or total_reward > torch.Tensor(reward_history):max() then
-            agent.best_network = agent.network:clone()
+            if intra_batch_index > batch_size then
+                if torch.random(test_fraction) == 1 then
+                    completed_test_batches = completed_test_batches + 1
+                    torch.save(dataset_output_dir .. '/test/images_batch_' .. completed_test_batches, images)
+                    torch.save(dataset_output_dir .. '/test/actions_batch_' .. completed_test_batches, actions)
+                else
+                    completed_train_batches = completed_train_batches + 1
+                    torch.save(dataset_output_dir .. '/train/images_batch_' .. completed_train_batches, images)
+                    torch.save(dataset_output_dir .. '/train/actions_batch_' .. completed_train_batches, actions)
+                end
+
+                intra_batch_index = 1
+                -- images = torch.Tensor(batch_size, 3, 210, 160)
+                -- actions = torch.Tensor(batch_size)
+            end
         end
 
-        if agent.v_avg then
-            v_history[ind] = agent.v_avg
-            td_history[ind] = agent.tderr_avg
-            qmax_history[ind] = agent.q_max
+
+        -- game over? get next game!
+        if not terminal then
+            screen, reward, terminal = game_env:step(game_actions[action_index], true)
+        else
+            if opt.random_starts > 0 then
+                screen, reward, terminal = game_env:nextRandomGame()
+            else
+                screen, reward, terminal = game_env:newGame()
+            end
         end
-        print("V", v_history[ind], "TD error", td_history[ind], "Qmax", qmax_history[ind])
 
-        reward_history[ind] = total_reward
-        reward_counts[ind] = nrewards
-        episode_counts[ind] = nepisodes
-
-        time_history[ind+1] = sys.clock() - start_time
-
-        local time_dif = time_history[ind+1] - time_history[ind]
-
-        local training_rate = opt.actrep*opt.eval_freq/time_dif
-
-        print(string.format(
-            '\nSteps: %d (frames: %d), reward: %.2f, epsilon: %.2f, lr: %G, ' ..
-            'training time: %ds, training rate: %dfps, testing time: %ds, ' ..
-            'testing rate: %dfps,  num. ep.: %d,  num. rewards: %d',
-            step, step*opt.actrep, total_reward, agent.ep, agent.lr, time_dif,
-            training_rate, eval_time, opt.actrep*opt.eval_steps/eval_time,
-            nepisodes, nrewards))
-    end
-
-    if step % opt.save_freq == 0 or step == opt.steps then
-       print(agent.network.modules[3].weight[1][1])
-        local s, a, r, s2, term = agent.valid_s, agent.valid_a, agent.valid_r,
-            agent.valid_s2, agent.valid_term
-        agent.valid_s, agent.valid_a, agent.valid_r, agent.valid_s2,
-            agent.valid_term = nil, nil, nil, nil, nil, nil, nil
-        local w, dw, g, g2, delta, delta2, deltas, tmp = agent.w, agent.dw,
-            agent.g, agent.g2, agent.delta, agent.delta2, agent.deltas, agent.tmp
-        agent.w, agent.dw, agent.g, agent.g2, agent.delta, agent.delta2,
-            agent.deltas, agent.tmp = nil, nil, nil, nil, nil, nil, nil, nil
-
-        local filename = opt.name
-        if opt.save_versions > 0 then
-            filename = filename .. "_" .. math.floor(step / opt.save_versions)
+        if step % opt.prog_freq == 0 then
+            -- print(screen)
+            assert(step==agent.numSteps, 'trainer step: ' .. step ..
+                    ' & agent.numSteps: ' .. agent.numSteps)
+            print("Steps: ", step)
+            agent:report()
+            collectgarbage()
         end
-        os.execute('mkdir -p networks')
-        filename = paths.concat('networks', filename)
-        torch.save(filename .. ".t7", {agent = agent,
-                                model = agent.network,
-                                best_model = agent.best_network,
-                                reward_history = reward_history,
-                                reward_counts = reward_counts,
-                                episode_counts = episode_counts,
-                                time_history = time_history,
-                                v_history = v_history,
-                                td_history = td_history,
-                                qmax_history = qmax_history,
-                                arguments=opt})
-        if opt.saveNetworkParams then
-            local nets = {network=w:clone():float()}
-            torch.save(filename..'.params.t7', nets, 'ascii')
-            print('Saved:', filename .. '.params.t7')
+
+        if step%1000 == 0 then collectgarbage() end
+
+        if step % opt.eval_freq == 0 and step > learn_start then
+
+            screen, reward, terminal = game_env:newGame()
+
+            total_reward = 0
+            nrewards = 0
+            nepisodes = 0
+            episode_reward = 0
+
+            local eval_time = sys.clock()
+            for estep=1,opt.eval_steps do
+                local action_index = agent:perceive(reward, screen, terminal, true, 0.05)
+
+                -- Play game in test mode (episodes don't end when losing a life)
+                screen, reward, terminal = game_env:step(game_actions[action_index])
+
+                if estep%1000 == 0 then collectgarbage() end
+
+                -- record every reward
+                episode_reward = episode_reward + reward
+                if reward ~= 0 then
+                   nrewards = nrewards + 1
+                end
+
+                if terminal then
+                    total_reward = total_reward + episode_reward
+                    episode_reward = 0
+                    nepisodes = nepisodes + 1
+                    screen, reward, terminal = game_env:nextRandomGame()
+                end
+            end
+
+            eval_time = sys.clock() - eval_time
+            start_time = start_time + eval_time
+            agent:compute_validation_statistics()
+            local ind = #reward_history+1
+            total_reward = total_reward/math.max(1, nepisodes)
+
+            if #reward_history == 0 or total_reward > torch.Tensor(reward_history):max() then
+                agent.best_network = agent.network:clone()
+            end
+
+            if agent.v_avg then
+                v_history[ind] = agent.v_avg
+                td_history[ind] = agent.tderr_avg
+                qmax_history[ind] = agent.q_max
+            end
+            print("V", v_history[ind], "TD error", td_history[ind], "Qmax", qmax_history[ind])
+
+            reward_history[ind] = total_reward
+            reward_counts[ind] = nrewards
+            episode_counts[ind] = nepisodes
+
+            time_history[ind+1] = sys.clock() - start_time
+
+            local time_dif = time_history[ind+1] - time_history[ind]
+
+            local training_rate = opt.actrep*opt.eval_freq/time_dif
+
+            print(string.format(
+                '\nSteps: %d (frames: %d), reward: %.2f, epsilon: %.2f, lr: %G, ' ..
+                'training time: %ds, training rate: %dfps, testing time: %ds, ' ..
+                'testing rate: %dfps,  num. ep.: %d,  num. rewards: %d',
+                step, step*opt.actrep, total_reward, agent.ep, agent.lr, time_dif,
+                training_rate, eval_time, opt.actrep*opt.eval_steps/eval_time,
+                nepisodes, nrewards))
         end
-        agent.valid_s, agent.valid_a, agent.valid_r, agent.valid_s2,
-            agent.valid_term = s, a, r, s2, term
-        agent.w, agent.dw, agent.g, agent.g2, agent.delta, agent.delta2,
-            agent.deltas, agent.tmp = w, dw, g, g2, delta, delta2, deltas, tmp
-        print('Saved:', filename .. '.t7')
-        io.flush()
-        collectgarbage()
+
+        if step % opt.save_freq == 0 or step == opt.steps then
+           print(agent.network.modules[3].weight[1][1])
+            local s, a, r, s2, term = agent.valid_s, agent.valid_a, agent.valid_r,
+                agent.valid_s2, agent.valid_term
+            agent.valid_s, agent.valid_a, agent.valid_r, agent.valid_s2,
+                agent.valid_term = nil, nil, nil, nil, nil, nil, nil
+            local w, dw, g, g2, delta, delta2, deltas, tmp = agent.w, agent.dw,
+                agent.g, agent.g2, agent.delta, agent.delta2, agent.deltas, agent.tmp
+            agent.w, agent.dw, agent.g, agent.g2, agent.delta, agent.delta2,
+                agent.deltas, agent.tmp = nil, nil, nil, nil, nil, nil, nil, nil
+
+            local filename = opt.name
+            if opt.save_versions > 0 then
+                filename = filename .. "_" .. math.floor(step / opt.save_versions)
+            end
+            os.execute('mkdir -p networks')
+            filename = paths.concat('networks', filename)
+            torch.save(filename .. ".t7", {agent = agent,
+                                    model = agent.network,
+                                    best_model = agent.best_network,
+                                    reward_history = reward_history,
+                                    reward_counts = reward_counts,
+                                    episode_counts = episode_counts,
+                                    time_history = time_history,
+                                    v_history = v_history,
+                                    td_history = td_history,
+                                    qmax_history = qmax_history,
+                                    arguments=opt})
+            if opt.saveNetworkParams then
+                local nets = {network=w:clone():float()}
+                torch.save(filename..'.params.t7', nets, 'ascii')
+                print('Saved:', filename .. '.params.t7')
+            end
+            agent.valid_s, agent.valid_a, agent.valid_r, agent.valid_s2,
+                agent.valid_term = s, a, r, s2, term
+            agent.w, agent.dw, agent.g, agent.g2, agent.delta, agent.delta2,
+                agent.deltas, agent.tmp = w, dw, g, g2, delta, delta2, deltas, tmp
+            print('Saved:', filename .. '.t7')
+            io.flush()
+            collectgarbage()
+        end
     end
 end
---]]
+
